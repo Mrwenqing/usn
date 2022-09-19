9a5594c57daf7886dc206eb5f5c0d37afc335e53
别再相遇了 <lg002825@126.con>
2022-09-19 20:06:06 +0800

    feat(custom): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    // 端口号
+    port: 8658,
+    // 主机名
+    host:"localhost",
+    // 启动项目时是否自动打开浏览器
+    open:true,
+    // 是否开启https
+    https:false,
+    // 配置跨域
+    proxy: {
+      [process.env.VUE_APP_BASE_API] :{
+        target: process.env.VUE._APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {[ '^' + process.env.VUE_APP_BASE_API ]:''}
+      }
+    }
+  },
+  // 关闭eslint
+  lintOnSave:false
 })
